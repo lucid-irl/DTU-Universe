d525de6ead2378fcecb857e71921ea74dd932915
truongaxin123 <truongaxin123@gmail.com>
2020-10-23 19:08:56 +0700

    add test cleanSchedule

---
+++ test_cleanSchedules.py
@@ -0,0 +1,25 @@
+import unittest
+from Schedule import Schedule
+
+
+class TestCleanSchedules(unittest.TestCase):
+
+    def setUp(self):
+        self.Schedule = Schedule([{'T2':['07:00-09:00','07:00-10:15']},{'T5':['07:00-09:00']}])
+        self.Schedule2 = Schedule([{'T3':['09:15-11:15']},{'T6':['09:15-11:15','07:00-10:15']}])
+
+    def test_GetNumberLessionPerWeek(self):
+        self.assertEqual(self.Schedule.getNumberLessonPerWeek(), 2)
+
+    def test_GetDatesOfLesson(self):
+        self.assertEqual(self.Schedule.getDatesOfLesson(), ["T2","T5"])
+    
+    def test_GetStudyTime1(self):
+        self.assertEqual(self.Schedule.getStudyTime(), 315.0)
+    
+    def test_GetStudyTime2(self):
+        self.assertEqual(self.Schedule2.getStudyTime(), 315.0)
+
+        
+if __name__ == "__main__":
+    unittest.main()
