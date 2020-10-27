c5ad1175cb01f411a3130ad56d5fb1f7dc7b4b62
truongaxin123 <truongaxin123@gmail.com>
2020-10-27 15:29:47 +0700

    edit semeter

--- Classes/semeter.py
+++ Classes/semeter.py
@@ -7,37 +7,44 @@ import random
 
 
 class Semeter:
+    """
+    Class này là class trung gian giữa Subject và Table
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Class này chịu trách nhiệm hiển thị trực quan lịch của một Subject lên Table. Các thao tác liên quan đến Table
+    có trên giao diện đều phải thông qua class này và các phương thức của nó.
+    """
 
-    time_chain = {'7:00:00':0,'9:00:00':1,'9:15:00':2,'10:15:00':3,'11:15:00':4,'13:00:00':5,'14:00:00':6,'15:00:00':7,'15:15:00':8,'16:15:00':9,'17:15:00':10,'17:45:00':11,'18:45:00':12,'21:00:00':13}
+    TIME_CHAINS = {'7:00:00':0,'9:00:00':1,'9:15:00':2,'10:15:00':3,'11:15:00':4,'13:00:00':5,'14:00:00':6,'15:00:00':7,'15:15:00':8,'16:15:00':9,'17:15:00':10,'17:45:00':11,'18:45:00':12,'21:00:00':13}
     color_choices = []
 
     def __init__(self, table: QTableWidget) -> None:
         self.table = table
-        self.subject = []
+        self.subjectss = []
 
     def getSubject(self):
-        return self.subject
+        return self.subjectss
 
     def addSubjectToSemeter(self, subject: Subject):
-        self.subject.append(subject)
-        self.LoadTable()
+        self.subjects.append(subject)
+        self.loadTable()
 
     def deleteSubject(self, name):
-        for j in range(len(self.subject)):
-            if self.subject[j].getName() == name:
-                self.subject.pop(j)
-        print(self.subject)
-        self.LoadTable()
-        self.removeAllItemTable()
+        for j in range(len(self.subjects)):
+            if self.subjects[j].getName() == name:
+                self.subjects.pop(j)
+        self.loadTable()
 
-    def removeAllItemTable(self):
+    def resetTableColor(self):
+        """Xoá hết màu có trên Table."""
         for i in range(self.table.rowCount()):
             for c in range(self.table.columnCount()):
                 self.table.setItem(i, c, QTableWidgetItem())
                 self.table.item(i, c).setBackground(QColor(255,255,255))
 
-    def LoadTable(self):
-        for subject1 in self.subject:
+    def loadTable(self):
+        """Quét lại bộ chứa subject và hiển thị lên Table."""
+        self.resetTableColor()
+        for subject1 in self.subjects:
             days = subject1.getSchedule().getDatesOfLesson()
             cl = random.choice(list_color)
             self.color_choices.append(cl)
@@ -48,8 +55,8 @@ class Semeter:
                 for i in range(len(start_time_subjects)):
                     start = str(start_time_subjects[i])
                     end = str(end_time_subjects[i])
-                    start_row = self.time_chain[start]
-                    end_row = self.time_chain[end]
+                    start_row = self.TIME_CHAINS[start]
+                    end_row = self.TIME_CHAINS[end]
                     column = WEEK.index(day)
                     for pen in range(start_row, end_row+1+1):
                         self.table.setItem(pen, column, QTableWidgetItem())
