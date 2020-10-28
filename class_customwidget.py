20b30037f2fda1a26a13a45bc214e6f24aafb731
MrSometimeswinmid <tranhuyhoang7@dtu.edu.vn>
2020-10-29 01:26:50 +0700

    edit customwidget

--- class_customwidget.py
+++ class_customwidget.py
@@ -14,7 +14,9 @@ class QCustomQWidget (QWidget):
         self.textQVBoxLayout = QHBoxLayout()
         self.text_subjectname = QLabel(subject.name, self.master)
         self.pixmap_status = QPixmap("Images/background.jpg")
+        
         self.label_status = QLabel(self.master)
+        self.label_status.images = self.pixmap_status
         self.label_status.setPixmap(self.pixmap_status)
         self.textQVBoxLayout.addWidget(self.text_subjectname)
         self.textQVBoxLayout.addWidget(self.label_status)
