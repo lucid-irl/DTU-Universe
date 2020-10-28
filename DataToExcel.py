6932a4d129144e347747caab48e6ec29161b407d
truongaxin123 <truongaxin123@gmail.com>
2020-10-28 10:56:30 +0700

    edit datatoexcel.py

--- DataToExcel.py
+++ DataToExcel.py
@@ -1,11 +1,14 @@
 from xlwt import Workbook
 from Crawl import *
-import os.path
 from os import path
 
+import os
+import team_config
+
+
 def CreateFolderToSave():
-    if path.exists("Data") == False:
-        os.mkdir("Data")
+    if path.exists(team_config.FOLDER_SAVE_EXCEL) == False:
+        os.mkdir(team_config.FOLDER_SAVE_EXCEL)
 
 def CreateExcel(name: str, number: str) -> bool:
     excel_name = name + " " + number + '.xls'
@@ -17,7 +20,6 @@ def CreateExcel(name: str, number: str) -> bool:
     except:
         return False
 
-    # hàng trước cột sau :
     sheet1.write(0, 0, "STT")
     sheet1.write(0, 1, "ID")
     sheet1.write(0, 2, "Name")
@@ -36,9 +38,9 @@ def CreateExcel(name: str, number: str) -> bool:
     col = 0
 
     for i in range(n):
-        sheet1.write(row + i, col, str(i + 1)) # Stt
-        sheet1.write(row + i, col + 1, GetID(soup)[i]) # ID
-        sheet1.write(row + i, col + 2, GetName(soup)[i]) # Name
+        sheet1.write(row + i, col, str(i + 1))
+        sheet1.write(row + i, col + 1, GetID(soup)[i])
+        sheet1.write(row + i, col + 2, GetName(soup)[i])
         sheet1.write(row + i, col + 3, GetSchedule(soup)[i])
         sheet1.write(row + i, col + 4, GetPlace(soup)[i])
         sheet1.write(row + i, col + 5, GetTeacher(soup)[i])
@@ -48,8 +50,5 @@ def CreateExcel(name: str, number: str) -> bool:
         sheet1.write(row + i, col + 9, GetStatus(soup)[i])
     
     CreateFolderToSave()
-    wb.save("Data/" + excel_name)
+    wb.save(team_config.FOLDER_SAVE_EXCEL+"/"+ excel_name)
     return True
-
-if __name__ == "__main__":
-    CreateExcel("ENG", "116")
