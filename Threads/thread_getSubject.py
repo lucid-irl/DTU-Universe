1b73ee826da80ed0d9d5666c1aedeaa3ad753f36
truongaxin123 <truongaxin123@gmail.com>
2020-10-28 21:31:27 +0700

    edit thread

--- Threads/thread_getSubject.py
+++ Threads/thread_getSubject.py
@@ -4,8 +4,8 @@ from PyQt5.QtCore import QThread, pyqtSignal
 from DataToExcel import CreateExcel
 
 class ThreadGetSubject(QThread):
-    foundExcel = pyqtSignal('PyQt_PyObject')
-    nonFoundExcel = pyqtSignal('PyQt_PyObject')
+    signal_foundExcel = pyqtSignal('PyQt_PyObject')
+    signal_nonFoundExcel = pyqtSignal('PyQt_PyObject')
 
     def __init__(self, name):
         QThread.__init__(self)
@@ -14,20 +14,20 @@ class ThreadGetSubject(QThread):
             self.sub = name.split(' ')[0]
             self.id = name.split(' ')[1]
         except:
-            self.nonFoundExcel.emit(False)
+            self.signal_nonFoundExcel.emit(False)
 
     
     def exist(self):
         if Get_Url(self.sub, self.id) == None:
-            self.nonFoundExcel.emit(False)
+            self.signal_nonFoundExcel.emit(False)
 
     def run(self):
         try:
             url = CreateExcel(self.sub, self.id)
         except:
-            self.nonFoundExcel.emit(False)
+            self.signal_nonFoundExcel.emit(False)
             return
         if url:
-            self.foundExcel.emit("Data/"+self.name+'.xls')
+            self.signal_foundExcel.emit("Data/"+self.name+'.xls')
         else:
-            self.nonFoundExcel.emit(False)
+            self.signal_nonFoundExcel.emit(False)
