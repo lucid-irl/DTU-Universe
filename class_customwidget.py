d9ded73af23ad2d84498e245fbf556d5c7120bed
MrSometimeswinmid <tranhuyhoang7@dtu.edu.vn>
2020-10-29 01:22:58 +0700

    add QpiC:/Users/WIN-PRO/AppData/Local/Programs/Python/Python36/python.exe c:/HuyHoang/Test/DoAn/main.py

--- class_customwidget.py
+++ class_customwidget.py
@@ -1,7 +1,7 @@
 
 from PyQt5.QtWidgets import QWidget,QLabel, QHBoxLayout, QLayout
 from class_subject import Subject
-
+from PyQt5.QtGui import QPixmap
 
 class QCustomQWidget (QWidget):
     """Custom layout cho item trong QListWidget."""
@@ -13,10 +13,14 @@ class QCustomQWidget (QWidget):
 
         self.textQVBoxLayout = QHBoxLayout()
         self.text_subjectname = QLabel(subject.name, self.master)
+        self.pixmap_status = QPixmap("Images/background.jpg")
+        self.label_status = QLabel(self.master)
+        self.label_status.setPixmap(self.pixmap_status)
         self.textQVBoxLayout.addWidget(self.text_subjectname)
+        self.textQVBoxLayout.addWidget(self.label_status)
         self.textQVBoxLayout.setSizeConstraint(QLayout.SetFixedSize)
         self.setLayout(self.textQVBoxLayout)
 
-        
-        
+    
+    
 
