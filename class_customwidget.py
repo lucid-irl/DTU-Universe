57c936d3c46889bff47b402e56402edfcfe44106
truongaxin123 <truongaxin123@gmail.com>
2020-11-01 21:54:26 +0700

    add Button add in CustomWidget

--- class_customwidget.py
+++ class_customwidget.py
@@ -1,11 +1,15 @@
-
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import pyqtSignal, Qt
 from PyQt5.QtWidgets import QApplication, QWidget,QLabel, QHBoxLayout, QLayout, QPushButton
-from class_subject import Subject
-from PyQt5.QtGui import QPixmap, QClipboard
 
-class QCustomQWidget (QWidget):
+from class_custombutton import PicButton
+from class_subject import Subject
+# from main import Main
+
+class QCustomQWidget(QWidget):
     """Custom layout cho item trong QListWidget."""
-    def __init__ (self,  subject: Subject, parent = None):
+
+    def __init__ (self, subject: Subject, parent=None):
         super(QCustomQWidget, self).__init__(parent)
         self.master = parent
         self.subject = subject
@@ -17,14 +21,17 @@ class QCustomQWidget (QWidget):
         if status == 0:
             self.pixmap_status = QPixmap(r'Images\red_dot.png')
             self.label_status.setPixmap(self.pixmap_status)
+            self.label_status.setToolTip('Không thể đăng ký')
         else:
             self.pixmap_status = QPixmap(r'Images\green_dot.png')
             self.label_status.setPixmap(self.pixmap_status)
+            self.label_status.setToolTip('Có thể đăng ký')
 
-        self.textQVBoxLayout.addWidget(self.label_status, 0)
-        self.textQVBoxLayout.addWidget(self.text_subjectname, 1)
+        self.textQVBoxLayout.addWidget(self.label_status, 0, Qt.AlignLeft)
+        self.textQVBoxLayout.addWidget(self.text_subjectname, 1, Qt.AlignLeft)
 
-        self.textQVBoxLayout.setSizeConstraint(QLayout.SetFixedSize)
+
+        self.textQVBoxLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
         self.setLayout(self.textQVBoxLayout)
 
     def addButtonCopyIDSubject(self):
@@ -37,6 +44,11 @@ class QCustomQWidget (QWidget):
         clipboard.clear(mode=clipboard.Clipboard)
         clipboard.setText(self.subject.getID())
 
-    
-    
+    def addButtonAddToSemeter(self):
+        self.button_add = PicButton(QPixmap('Images\icon_add.png'),QPixmap('Images\icon_add.png'),QPixmap('Images\icon_add.png'),self)
+        self.textQVBoxLayout.addWidget(self.button_add, 0, Qt.AlignRight)
+        self.button_add.clicked.connect(self.addThisSubjectToSemeter)
+        self.button_add.setToolTip('Thêm môn này vào bảng')
 
+    def addThisSubjectToSemeter(self):
+        self.master.addSubjectToTable(self.subject)
