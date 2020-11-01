abddbad73d754c54adeff1577f6ddd9b4fac8005
truongaxin123 <truongaxin123@gmail.com>
2020-11-01 21:55:58 +0700

    update team config

--- team_config.py
+++ team_config.py
@@ -4,3 +4,7 @@ FOLDER_SAVE_EXCEL = "Data"
 # Nơi chưa tệp UI
 FOLDER_UI = "GUI"
 USE_UI = "main.ui"
+
+# Message cảnh báo
+MESSAGE_ABOUT = 'Một tin nhắn sương sương'
+MESSAGE_WARNING = 'Một cảnh báo sương sương'
