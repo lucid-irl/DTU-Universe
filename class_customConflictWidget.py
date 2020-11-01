99628d14d3bd5a689fbead8bb6fb5a4b12d88275
truongaxin123 <truongaxin123@gmail.com>
2020-11-01 21:54:02 +0700

    add conflict widget file

---
+++ class_customConflictWidget.py
@@ -0,0 +1,28 @@
+from class_subject import Subject
+from PyQt5.QtWidgets import QWidget, QHBoxLayout,  QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+
+class CustomConflictWidget(QWidget):
+
+    def __init__(self, subject1: Subject, subject2: Subject, parent=None) -> None:
+        super(CustomConflictWidget, self).__init__(parent=parent)
+        self.subject1 = subject1
+        self.subject2 = subject2
+        self.setupUI()
+
+    def setupUI(self):
+        self.hboxlayout = QHBoxLayout()
+        self.label_subjectName1 = QLabel(self.subject1.getName(), self)
+        self.label_subjectName1.setAlignment(Qt.AlignCenter)
+        self.label_subjectName2 = QLabel(self.subject2.getName(), self)
+        self.label_subjectName2.setAlignment(Qt.AlignCenter)
+        self.pixmap_icon = QPixmap(r'Images\red_dot.png')
+        self.label_image = QLabel(self)
+        self.label_image.setPixmap(self.pixmap_icon)
+        self.label_image.setAlignment(Qt.AlignCenter)
+        self.hboxlayout.addWidget(self.label_subjectName1, 2)
+        self.hboxlayout.addWidget(self.label_image, 1)
+        self.hboxlayout.addWidget(self.label_subjectName2, 2)
+        self.setLayout(self.hboxlayout)
