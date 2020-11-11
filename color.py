2463d86c835bf051ce329bae1deabd828471266f
MrSometimeswinmid <tranhuyhoang7@dtu.edu.vn>
2020-11-11 22:59:58 +0700

    add random color

--- color.py
+++ color.py
@@ -1,5 +1,5 @@
 import re
-
+import random
 
 mint_leaf = '#00b894'
 light_blue = '#00cec9'
@@ -29,3 +29,22 @@ def getColor():
     color = list_color[0]
     list_color.pop(0)
     return color
+
+
+def hex_code_colors():
+    a = hex(random.randrange(0,256))
+    b = hex(random.randrange(0,256))
+    c = hex(random.randrange(0,256))
+    a = a[2:]
+    b = b[2:]
+    c = c[2:]
+    if len(a)<2:
+        a = "0" + a
+    if len(b)<2:
+        b = "0" + b
+    if len(c)<2:
+        c = "0" + c
+    z = a + b + c
+    return "#" + z.upper()
+
+
