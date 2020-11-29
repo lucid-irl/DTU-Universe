dbf1dc5f1026bfa480a05d9d8091a1dde7bb1b5b
truongaxin123 <truongaxin123@gmail.com>
2020-11-29 22:51:57 +0700

    edit color

--- color.py
+++ color.py
@@ -1,34 +1,4 @@
-import re
 import random
-
-mint_leaf = '#00b894'
-light_blue = '#00cec9'
-elec_blue = '#0984e3'
-purple = '#6c5ce7'
-silver = '#b2bec3'
-light_yellow = '#ffeaa7'
-light_pink = '#ff7675'
-orange = '#e17055'
-red = '#d63031'
-pink = '#e84393'
-
-list_z = [
-    mint_leaf,
-    light_blue,
-    elec_blue,
-    purple,
-    silver,
-    light_yellow,
-    light_pink,
-    orange,
-    red,
-    pink
-]
-
-def getz():
-    z = list_z[0]
-    list_z.pop(0)
-    return z
 
 LIST_COLORS = []
 
@@ -59,12 +29,6 @@ def hex_code_colors():
             continue
         LIST_COLORS.append(z)
         return z
-
-
-
-if __name__ == "__main__":
-    print(LIST_COLORS)
-    print(hex_code_colors())
 
 
 
