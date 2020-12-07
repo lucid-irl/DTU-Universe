78636417044bf46cd0b5a40b4c9d8a8967b77b8b
truongaxin123 <truongaxin123@gmail.com>
2020-12-07 13:36:04 +0700

    add app label captcha

---
+++ app_label.py
@@ -0,0 +1,58 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+import os
+import sys
+from class_convertType import ConvertThisQObject
+import crawl_captcha
+import re
+
+class App(QWidget):
+
+    FOLDER_SRC = 'resolved_captchas'
+
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent=parent)
+        if os.path.exists(self.FOLDER_SRC) == False:
+            os.mkdir(self.FOLDER_SRC)
+        uic.loadUi('label_app.ui', self)
+        self.button_next = ConvertThisQObject(self, QPushButton, 'button_next').toQPushButton()
+        self.edit_text = ConvertThisQObject(self, QLineEdit, 'edit_text').toQLineEdit()
+        self.label_captcha = ConvertThisQObject(self, QLabel, 'label_captcha').toQLabel()
+        self.load_captcha()
+        self.connectSignal()
+        self.connectHotKey()
+    
+    def connectSignal(self):
+        self.button_next.clicked.connect(self.next)
+
+    def connectHotKey(self):
+        self.button_next.setShortcut('Return')
+    
+    def load_captcha(self):
+        self.edit_text.setFocus()
+        captcha_id = crawl_captcha.get_captcha_id()
+        crawl_captcha.download_captcha_image(captcha_id, self.FOLDER_SRC)
+        self.file_name = self.FOLDER_SRC+'\\'+captcha_id+'.jpeg'
+        self.image = QPixmap(self.file_name)
+        self.label_captcha.setPixmap(self.image)
+
+    def next(self):
+        captcha_text = self.edit_text.text().upper()
+        if re.search('[A-Z0-9][A-Z0-9][A-Z0-9][A-Z0-9]', captcha_text):
+            new_name = self.FOLDER_SRC+'\\'+captcha_text+'.jpeg'
+            os.rename(self.file_name, new_name)
+
+        else:
+            print('invalid text')
+            os.remove(self.file_name)
+        self.load_captcha()
+        self.edit_text.clear()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+    window.show()
+    sys.exit(app.exec())
