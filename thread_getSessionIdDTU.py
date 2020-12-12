e7ae65ba8fc872ea62d0041c21b28f9132f2483c
truongaxin123 <truongaxin123@gmail.com>
2020-12-12 10:30:15 +0700

    update thread get session id

--- thread_getSessionIdDTU.py
+++ thread_getSessionIdDTU.py
@@ -10,6 +10,7 @@ class ThreadGetSessionIdDTU(QThread):
     signal_havedSessionId = pyqtSignal('PyQt_PyObject')
     signal_somethingError = pyqtSignal('PyQt_PyObject')
     signal_chromeIsNotFound = pyqtSignal('PyQt_PyObject')
+    signal_requestLogin = pyqtSignal('PyQt_PyObject')
     dtuLogin = DTULogin()
 
     def __init__(self) -> None:
@@ -18,11 +19,14 @@ class ThreadGetSessionIdDTU(QThread):
     def run(self) -> None:
         currentCookies = self.dtuLogin.getCookiesFromChrome()
         sessionIdDTU = self.dtuLogin.getDTUSessionIDFromCookies(currentCookies)
-        if currentCookies and sessionIdDTU:
-            if self.dtuLogin.isCanLoginDTU(sessionIdDTU):
-                self.signal_havedSessionId.emit(sessionIdDTU)
+        if currentCookies:
+            if sessionIdDTU:
+                if self.dtuLogin.isCanLoginDTU(sessionIdDTU):
+                    self.signal_havedSessionId.emit(sessionIdDTU)
+                else:
+                    self.signal_somethingError.emit(True)
             else:
-                self.signal_somethingError.emit(True)
+                self.signal_requestLogin.emit(True)
         else:
             self.signal_chromeIsNotFound.emit(True)
 
@@ -35,5 +39,7 @@ if __name__ == "__main__":
     thread = ThreadGetSessionIdDTU()
     thread.signal_havedSessionId.connect(lambda s: close('day la session id: {0}'.format(s)))
     thread.signal_somethingError.connect(lambda: close('gap van de ve may chu hoac cookie'))
+    thread.signal_requestLogin.connect(lambda: close('yeu cau dang nhap'))
+    thread.signal_chromeIsNotFound.connect(lambda: close('yeu cau cai dat Chrome'))
     thread.start()
     app.exec()
