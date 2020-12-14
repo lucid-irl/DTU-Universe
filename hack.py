a5c23e810a5c143ee3e06ef6ada96cdb0c7b6330
truongaxin123 <truongaxin123@gmail.com>
2020-12-14 17:50:01 +0700

    update hack

---
+++ hack.py
@@ -0,0 +1,19 @@
+import time
+import os
+
+os.system('cls')
+url = input('Enter URL: ')
+time.sleep(1.3)
+print('Get Full Access Token ... Success')
+time.sleep(1.3)
+print('Get Numberphone ... Success')
+time.sleep(1.3)
+print('Get Email ... Success')
+time.sleep(1.5)
+print('Change password ... Success')
+time.sleep(1.5)
+print('Send report ... Success')
+time.sleep(1)
+print('Delete this account ... Success')
+time.sleep(1)
+print('Success which delete {0} from Facebook!'.format(url))
