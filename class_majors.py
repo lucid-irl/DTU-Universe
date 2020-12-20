d0de5f5e6edd0a4becdd8de75e0d96b47a80b43f
MrSometimeswinmid <tranhuyhoang7@dtu.edu.vn>
2020-12-21 03:58:37 +0700

    add class_majors

--- class_majors.py
+++ class_majors.py
@@ -1,3 +1,4 @@
+import os
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 import json
@@ -8,7 +9,7 @@ def getDivSemester(html : str) -> list:
     div_tag = divSoup.find_all('div', class_ = 'semeter')
     return div_tag
 
-def getSemestersInfor(html : Tag) -> dict:
+def getSemestersInfor(html : Tag, id_) -> dict:
     semestersInfor = html.find('th', class_ = 'nobg nobd').text
     semestersInfor = str(semestersInfor).strip()
     tbody = html.find_all('tbody')[0]
@@ -18,23 +19,38 @@ def getSemestersInfor(html : Tag) -> dict:
     listSubject = []
     for trTag in listTrTag:
         id = str(trTag('td')[0].a.text).strip()
-        
         name = str(trTag('td')[1].a.text).strip()
         credit = str(trTag('td')[2].text).strip()
         subject = {'id':id, 'name':name,'credit':credit}
         listSubject.append(subject)
     return {
+        'keyid':id_,
         'semester':semestersInfor,
         'subjects':listSubject
     }
 
+def getHTMLFromFile(path) -> str:
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
 
-if __name__ == "__main__":
-    filename = "majors\Big Data & Machine Learning (Đại Học - HP).html"
-    with open(filename,'r',encoding = 'utf-8') as f:
-        data = f.read()
-        listSemester = []
-        for div in getDivSemester(data):
-            listSemester.append(getSemestersInfor(div))
-    with open('info.json','w',encoding='utf-8') as g:
-        json.dump(listSemester, g, indent=4, ensure_ascii=False)
+def getListInfo():
+    pass
+def main():
+    majorHtmlPaths = ['majors/' + i for i in os.listdir('majors')]
+    listMajor = []
+    for k in range(len(majorHtmlPaths)):
+        html = getHTMLFromFile(majorHtmlPaths[k])
+        listSemesterDiv = getDivSemester(html)
+        listInfoes = []
+        for i in range(len(listSemesterDiv)):
+            info = getSemestersInfor(listSemesterDiv[i], id_=i)
+            listInfoes.append(info)
+        major = majorHtmlPaths[k].split('/')[1].split('.')[0]
+        
+        listMajor.append({'id':k, 'major': major, 'semester_info':listInfoes})
+    return listMajor
+
+
+js = main()
+with open('info.json','w',encoding='utf-8') as g:
+    json.dump(js, g, indent=4, ensure_ascii=False)
