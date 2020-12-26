fbdda998b07c4f07bdc0444458aa1b6263af982d
truongaxin123 <truongaxin123@gmail.com>
2020-12-26 11:44:21 +0700

    edit team config

--- team_config.py
+++ team_config.py
@@ -6,10 +6,16 @@ SHORT_NAME = 'CS4RSA'
 FOLDER_SAVE_EXCEL = "Data"
 
 # Nơi chưa tệp UI
-FOLDER_UI = "GUI"
+FOLDER_UI = "guis"
 USE_UI = "main.ui"
 CALENDAR_CHOICE_UI = 'calendarChoicer.ui'
 PREDICT_SUBJECT = 'predict_subject.ui'
+DOWNLOAD_SUBJECT = 'window_load_subject.ui'
+
+UI_DOWNLOAD_SUJECT = FOLDER_UI+'/'+DOWNLOAD_SUBJECT
+
+
+FOLDER_FONT = 'fonts'
 
 # RELATIVE PATH UI
 USE_UI_PATH = FOLDER_UI+'/'+USE_UI
@@ -22,5 +28,3 @@ MESSAGE_WARNING = 'Một cảnh báo sương sương'
 # Message
 MESSAGE_UPDATE_CONTENT = 'Khi update tất cả các file excel có trong thư mục {0} sẽ bị xoá, tất cả các môn bạn đã chọn sẽ được tải lại. Quá trình này sẽ tốn một chút thời gian, bạn có chắc muốn cập nhật!'. format(FOLDER_SAVE_EXCEL)
 MESSAGE_DONATE_CONTENT = 'Một số tính năng bạn cần donate tương ứng để mở khoá. Việc phần mềm này free như cách bạn hít thở không khí trong lành vậy, nó là điều tất yếu. Nhưng để trong nhà có không khí trong lành bạn nên mua Máy Lọc Không Khí.'
-
-# Error
