d053a5b51ae479336a2ce19ac0214b130f0b295a
truongaxin123 <truongaxin123@gmail.com>
2021-01-24 11:46:12 +0700

    register

--- class_DTURegister.py
+++ class_DTURegister.py
@@ -1,19 +1,5 @@
-import asyncio
+from class_dialogNotification import NotificationWindow
 from PyQt5.QtCore import QThread, pyqtSignal
-
-async def sendRequest():
-    print('Send request register!!!')
-    await asyncio.sleep(4)
-    print('Success')
-
-async def register(subject: str):
-    print(subject)
-    await sendRequest()
-    return 1
-
-async def main():
-    subjects = ['CS 132', 'HG123', 'OJU 412']
-    await asyncio.gather(*(register(subject) for subject in subjects))
 
 class ThreadDTURegister(QThread):
     signal_Done = pyqtSignal('PyQt_PyObject')
@@ -22,5 +8,4 @@ class ThreadDTURegister(QThread):
         QThread.__init__(self)
 
     def run(self) -> None:
-        r = asyncio.run(main())
-        self.signal_Done.emit(r)
+        NotificationWindow('Thật là buồn','Đây là một tính năng thử nghiệm và sẽ đéo bao giờ có trên đời này, huhu').exec()
